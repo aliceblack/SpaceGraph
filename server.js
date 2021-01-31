3a0afda82b7cb1807197005940b0524971fca0c8
aliceblack <blackfluid44@gmail.com>
2021-01-31 14:19:10 +0100

    GraphQL

--- server.js
+++ server.js
@@ -10,9 +10,13 @@ app.use(bodyParser.json());
 var cors = require('cors');
 app.use(cors());
 
+
+
 // Create link to Angular build directory
 var distDir = __dirname + "/dist/spacegraph";
 app.use(express.static(distDir));
+
+/* REST version */
 
 app.get('/missions', (req, res) => {
     getMissionsFromSpaceX()
@@ -30,13 +34,114 @@ function getMissionsFromSpaceX(){
         var url = 'https://api.spacexdata.com/v3/launches';
         axios.get(url)
         .then(response => {
-            resolve(response.data);
+            resolve({data:{ launches: response.data}});
         })
         .catch(error => {
           reject(error);
         });
     });
 }
+
+
+/* GraphQL version */
+
+var { graphql, buildSchema } = require('graphql');
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
+
+const {
+    GraphQLObjectType,
+    GraphQLInt,
+    GraphQLString,
+    GraphQLBoolean,
+    GraphQLList,
+    GraphQLSchema
+} = require('graphql');
+
+const LaunchType = new GraphQLObjectType({
+    name: 'Launch',
+    fields: () => ({
+        mission_name: { type: GraphQLString },
+        launch_date_utc: { type: GraphQLString },
+        launch_success: { type: GraphQLBoolean },
+        details: { type: GraphQLString },
+        flight_number: { type: GraphQLInt },
+        rocket: { type: RocketType },
+        links: { type: LinksType },
+    })
+});
+
+const RocketType = new GraphQLObjectType({
+    name: 'Rocket',
+    fields: () => ({
+      rocket_id: { type: GraphQLString },
+      rocket_name: { type: GraphQLString },
+      rocket_type: { type: GraphQLString }
+    })
+});
+
+const LinksType = new GraphQLObjectType({
+    name: 'Link',
+    fields: () => ({
+        mission_patch: { type: GraphQLString },
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      launches: {
+        type: new GraphQLList(LaunchType),
+        resolve(parent, args) {
+          return axios
+            .get('https://api.spacexdata.com/v3/launches')
+            .then(res => res.data);
+        }
+      },
+      launch: {
+        type: LaunchType,
+        args: {
+          flight_number: { type: GraphQLInt }
+        },
+        resolve(parent, args) {
+          return axios
+            .get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
+            .then(res => res.data);
+        }
+      },
+      rockets: {
+        type: new GraphQLList(RocketType),
+        resolve(parent, args) {
+          return axios
+            .get('https://api.spacexdata.com/v3/rockets')
+            .then(res => res.data);
+        }
+      },
+      rocket: {
+        type: RocketType,
+        args: {
+          id: { type: GraphQLInt }
+        },
+        resolve(parent, args) {
+          return axios
+            .get(`https://api.spacexdata.com/v3/rockets/${args.id}`)
+            .then(res => res.data);
+        }
+      }
+    }
+});
+
+var schema = new GraphQLSchema({
+    query: RootQuery
+});
+
+app.use(
+    '/graphql',
+    graphqlHTTP({
+      schema,
+      graphiql: true
+    })
+);
+
 
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,13 +19,21 @@ export class AppComponent {
   missions;
   setMissions(){
     this.error=false;
-    this.getMissions()
+    this.getMissionsGraphQL()
     .toPromise()
-    .then(missions=>{this.missions=missions})
+    .then(missions=>{this.missions=missions["data"]["launches"]})
     .catch(error=>{console.log(error); this.error=true;});
   }
 
-  getMissions(){
+  getMissionsREST(){
     return this.http.get("/missions");
+  }
+
+  getMissionsGraphQL(){
+    let requestBody = {
+      "query": "{launches{mission_name,launch_date_utc,details,rocket{rocket_name,rocket_type},links{mission_patch}}}",
+      "variables": null
+    };
+    return this.http.post("/graphql?", requestBody);
   }
 }
